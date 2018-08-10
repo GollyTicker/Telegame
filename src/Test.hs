d97cd12429a68b0f1503e0bddf5f30110effe471
GollyTicker <golly.ticker@gmail.com>
2018-08-10 23:15:18 +0200

    added HUnit

--- src/Test.hs
+++ src/Test.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 import Test.QuickCheck
+import Test.HUnit
 import Base
 import View()
 import Control.Applicative (liftA2,liftA3)
@@ -9,6 +10,12 @@ import Data.MultiSet (MultiSet)
 import Data.Char (toUpper)
 
 -- QuickCheck overview: http://www.cse.chalmers.se/~rjmh/QuickCheck/manual_body.html#16
+
+-- HUNIT: http://hackage.haskell.org/package/HUnit-1.6.0.0/docs/Test-HUnit.html
+
+
+htests = TestList []
+
 
 instance Arbitrary Dir where
   arbitrary = elements [L,U,R,D]
@@ -24,6 +31,12 @@ instance (Ord a, Arbitrary a) => Arbitrary (MultiSet a) where
     MS.fromList <$> vector n
 ;
 
+instance Arbitrary Env where
+  arbitrary = do
+    oneof [do n <- elements [0..4]; h <- elements [0..n]; return $ Door n h,
+           return Solid,return Blank,return Platform,
+           Switch <$> arbitrary]
+
 nameStrGen = elements [1..5] >>= \n -> vectorOf n (oneof [pure id, pure toUpper] <*> elements ['a'..'h'])
 
 playerGen :: Gen Player
@@ -36,9 +49,21 @@ prop_read_show_Dir d = d == read (show d)
 prop_read_show_PhyObj :: PhyObj ->Bool
 prop_read_show_PhyObj d = d == read (show d)
 
+prop_read_show_Env :: Dir -> Bool
+prop_read_show_Env e = e == read (show e)
+
 prop_read_show_Player :: Property
 prop_read_show_Player = forAll playerGen $ \d -> d == read (show d)
 
 
 return []
-main = $(quickCheckAll) >> return ()
+main = putStrLn "QuickCkeck:\n"
+  >> $(quickCheckAll)
+  >> putStrLn "HUnit:\n"
+  >> runTestTT htests >> return ()
+
+
+
+
+
+
